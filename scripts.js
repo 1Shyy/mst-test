b7c1dd7db3c4452330021cc85db83249af2efdf0
Nikita Sobolev <wwe.sobolev@yandex.ru>
2024-10-14 07:11:03 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 99) {
+        this.document.getElementById('header').classList.add("header-back")
+    } else {
+        this.document.getElementById('header').classList.remove("header-back")
+    }
+  });
